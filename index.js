ec72c7b3fccba646c1d0557955cea7eaedfc2903
d3fcom <weis23.lp@gmail.com>
2022-07-17 17:42:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Accounts = require('web3-eth-accounts');
 var accounts = new Accounts();
 
 let new_account;
-for (let i = 0; i < 75; i++) {
+for (let i = 0; i < 1000; i++) {
   new_account = accounts.create();
   console.log({
     'private_key': new_account['privateKey'],
